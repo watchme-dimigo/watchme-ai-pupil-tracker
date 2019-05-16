bc1e54a3e8811b2c107c709da333768eb9a29386
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-05-17 07:15:31 +0900

    fix: use predict_classes

--- prediction.py
+++ prediction.py
@@ -12,10 +12,10 @@ def prediction(img_path, model_path='./model.h5'):
     img = np.expand_dims(image.img_to_array(img), axis=0)
     # print(img)
 
-    result = model.predict(img)
-    return result
+    result = model.predict_classes(img)
+    return ['bottom_left', 'bottom_right', 'normal', 'top_left', 'top_right'][result[0]]
 
 if __name__ == '__main__':
     res = prediction(f'./test.png')
     print(res)
-  
+    
